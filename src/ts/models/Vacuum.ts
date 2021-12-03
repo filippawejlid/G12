eea73c39c2aab6ca442fec12ac294d521dd55eb4
Filippa <filippabellange@live.se>
2021-12-03 10:23:48 +0100

    category

--- src/ts/models/Vacuum.ts
+++ src/ts/models/Vacuum.ts
@@ -4,12 +4,14 @@ class Vacuum {
     price: number;
     model: string;
     id: number;
+    category: string;
 
-    constructor (name: string, price: number, model: string, Id: number) { 
+    constructor (name: string, price: number, model: string, Id: number, category: string) { 
 
         this.productName = name;
         this.price = price;
         this.model = model;
         this.id = Id;
+        this.category = category;
     }
 }
