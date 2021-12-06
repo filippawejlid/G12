8041f83c9404af31747f13d9a9cb487d8bc8fa32
Filip Wieselgren <filipwieselgren@gmail.com>
2021-12-06 13:00:29 +0100

    Fixat produktobjekten

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,30 @@
+import { Vacuum } from "./models/Vacuum";
+import {
+  w1,
+  w2,
+  w3,
+  w4,
+  p1,
+  p2,
+  p3,
+  p4,
+  r1,
+  r2,
+  r3,
+  r4,
+} from "./models/Vacuumobjects";
+let productInfo: Vacuum[] = [w1, w2, w3, w4, p1, p2, p3, p4, r1, r2, r3, r4];
 window.onload = function () {
-	// cartClick();
-	clicker();
+  // FOR LOOP FOR ALL THE PRODUCTS ON THE FRONT PAGE
+
+  for (let i = 0; i < productInfo.length; i++) {
+    let imgContainer: HTMLDivElement = document.getElementById(
+      "imgcontainer"
+    ) as HTMLDivElement;
+    imgContainer.innerHTML = productInfo[i].imgsmall;
+  }
+  // cartClick();
+  clicker();
 };
 
 // function cartClick() {
@@ -18,69 +42,34 @@ window.onload = function () {
 // }
 
 function clicker() {
-	var modal: HTMLDivElement = document.getElementById(
-		"myModal"
-	) as HTMLDivElement;
+  var modal: HTMLDivElement = document.getElementById(
+    "myModal"
+  ) as HTMLDivElement;
 
-	// Get the button that opens the modal
-	var btn: HTMLButtonElement = document.getElementById(
-		"btnCart"
-	) as HTMLButtonElement;
+  // Get the button that opens the modal
+  var btn: HTMLButtonElement = document.getElementById(
+    "btnCart"
+  ) as HTMLButtonElement;
 
-	// Get the <span> element that closes the modal
-	var span: HTMLSpanElement = document.getElementsByClassName(
-		"close"
-	)[0] as HTMLSpanElement;
+  // Get the <span> element that closes the modal
+  var span: HTMLSpanElement = document.getElementsByClassName(
+    "close"
+  )[0] as HTMLSpanElement;
 
-	// When the user clicks on the button, open the modal
-	btn.onclick = function () {
-		modal.style.display = "block";
-	};
+  // When the user clicks on the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
 
-	// When the user clicks on <span> (x), close the modal
-	span.addEventListener("click", function () {
-		modal.style.display = "none";
-	});
+  // When the user clicks on <span> (x), close the modal
+  span.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	};
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
-import { Vacuum } from "./models/Vacuum";
-
-window.onload = () => {
-	let w1 = new Vacuum(
-		"Dyson Cyclone V10",
-		5190,
-		"/assets/Dysoncyclonev10_productpage.png",
-		"<img src=/src/assets/Sladdlös-Dyson-Cyclone-V10.png alt='Bild på en dammsugaren' width='200' height='250'/>",
-		"<h2>Dyson Cyclone V10</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
-		"<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
-		"<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
-		1,
-		"wireless"
-	);
-	let productInfo: Vacuum[] = [w1];
-
-	for (let i = 0; i < productInfo.length; i++) {
-		let imgContainer: HTMLDivElement = document.getElementById(
-			"imgcontainer"
-		) as HTMLDivElement;
-		imgContainer.innerHTML = productInfo[i].imgsmall;
-	}
-};
-
-// let w2 = new Vacuum("Dyson Outsize", 8290);
-// let w3 = new Vacuum("Dyson V12 detect slim Complete", 7190);
-// let w4 = new Vacuum("Dyson V15 Detect Absolute", 7190);
-// let p1 = new Vacuum("Dyson Big Ball Multi Floor", 3190);
-// let p2 = new Vacuum("Dyson Cinetic Big Ball Absolute", 4190);
-// let p3 = new Vacuum("Dyson Cinetic Big Ball Animal Pro 1", 4190);
-// let p4 = new Vacuum("Dyson Cinetic Big Ball Animal Pro 2", 5190, "<h2>");
-// let r1 = new Vacuum("Dyson 360 Heurist™ robot 1", 3199);
-// let r2 = new Vacuum("Dyson 360 Heurist™ robot 2", 3999);
-// let r3 = new Vacuum("Dyson 360 Heurist™ robot 3", 4190);
-// let r4 = new Vacuum("Dyson 360 Heurist™ robot 4", 4999);
---
+++ src/ts/models/Vacuumobjects.ts
@@ -0,0 +1,141 @@
+import { Vacuum } from "./Vacuum";
+
+export let w1 = new Vacuum(
+  "Dyson Cyclone V10",
+  5190,
+  "<img src=/src/assets/Dysoncyclonev10_productpage.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Sladdlös-Dyson-Cyclone-V10.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cyclone V10</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  1,
+  "wireless"
+);
+export let w2 = new Vacuum(
+  "Dyson Outsize",
+  8290,
+  "<img src=/src/assets/Sladdlös-Dyson-Outsize.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Sladdlös-Dyson-Outsize.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Outsize</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  2,
+  "wireless"
+);
+
+export let w3 = new Vacuum(
+  "Dyson V12 detect slim Complete",
+  7190,
+  "<img src=/src/assets/Sladdlös-Dyson-V12-detect-slim-Complete.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Sladdlös-Dyson-V12-detect-slim-Complete.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson V12 detect slim Complete</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  3,
+  "wireless"
+);
+
+export let w4 = new Vacuum(
+  "Dyson V15 Detect Absolute",
+  7190,
+  "<img src=/src/assets/Sladdlös-Dyson-V15-Detect-Absolute-logo.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Sladdlös-Dyson-V15-Detect-Absolute-logo.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson V15 Detect Absolute</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  4,
+  "wireless"
+);
+
+export let p1 = new Vacuum(
+  "Dyson Big Ball Multi Floor",
+  3190,
+  "<img src=/src/assets/Kraftfull-Dyson-Big-Ball-Multi-Floor-2.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Kraftfull-Dyson-Big-Ball-Multi-Floor-2.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Big Ball Multi Floor</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  5,
+  "powerfull"
+);
+
+export let p2 = new Vacuum(
+  "Dyson Cinetic Big Ball Absolute",
+  4190,
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Absolute-2.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Absolute-2.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Absolute</h2> <span> Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  6,
+  "powerfull"
+);
+
+export let p3 = new Vacuum(
+  "Dyson Cinetic Big Ball Animal Pro 1",
+  4190,
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Animal-Pro-1.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Animal-Pro-1.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 1</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  7,
+  "powerfull"
+);
+
+export let p4 = new Vacuum(
+  "Dyson Cinetic Big Ball Animal Pro 2",
+  5190,
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Animal-Pro-2.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Kraftfull-Dyson-Cinetic-Big-Ball-Animal-Pro-2.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 2</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  8,
+  "powerfull"
+);
+
+export let r1 = new Vacuum(
+  "Dyson 360 Heurist™ robot 1",
+  3199,
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-1.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-1.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 2</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  9,
+  "powerfull"
+);
+export let r2 = new Vacuum(
+  "Dyson 360 Heurist™ robot 2",
+  3999,
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-2.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-2.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 2</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  10,
+  "powerfull"
+);
+export let r3 = new Vacuum(
+  "Dyson 360 Heurist™ robot 3",
+  4199,
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-3.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-3.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 2</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  11,
+  "powerfull"
+);
+export let r4 = new Vacuum(
+  "Dyson 360 Heurist™ robot 4",
+  4999,
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-4.png alt='Bild på en dammsugaren'/>",
+  "<img src=/src/assets/Robot-Dyson-360-Heurist™-robot-4.png alt='Bild på en dammsugaren' width='200' height='250'/>",
+  "<h2>Dyson Cinetic Big Ball Animal Pro 2</h2> <span>Byggt för djup rengöring, överallt. Upp till 60 minuter körtid.</span>",
+  "<h1>Oslagbar prestanda</h1> <div>Dyson Cyclone V10 Absolute kommer med upp till 60 minuters kraftfull bibehållen sugförmåga. Den 40% större behållaren betyder också färre avbrott. </div>",
+  "<h1>Avancerad filtrering</h1> <div>Avancerad filtrering säkerställer att dammsugaren släpper ut renare luft och tömningssystemet 'point and shoot' möjliggör en mer hygienisk metod för att tömma behållaren. Inkluderar åtta verktyg och tillbehör för grundlig rengöring var som helst i hemmet.</div>",
+  12,
+  "powerfull"
+);
