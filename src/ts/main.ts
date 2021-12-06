09526bb781c56bc68e5ea62100231e66801d5a2c
Filip Wieselgren <filipwieselgren@gmail.com>
2021-12-06 20:22:21 +0100

    Skpade html via typesrcipt

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -13,19 +13,94 @@ import {
   r3,
   r4,
 } from "./models/Vacuumobjects";
+// <div id="wirelesscontainer">
+//  <div class="productcontainer">
+//               <div class="imgcontainer">
+//                   <img />
+//               </div>
+//               <div class="productInfoContainer">
+//                 <h2 class="vacuumheader"></h2>
+//                 <div class="vacuuminfo">
+//                   <span></span>
+
+//                 </div>
+//                  <h2 class="price"></h2>
+//                 <div class="productbtns">
+//                   <button class="readmore" id="dysoncyclonev10btn" data.value="dysoncyclonev10btn" >Läs mer <i class="fas fa-info-circle"></i></button>
+//                   <button class="addtocart">Lägg till i varukorg <i class="fas fa-cart-plus"></i></button>
+//                 </div>
+//               </div>
+//   </div>
+// </div>
+
 let productInfo: Vacuum[] = [w1, w2, w3, w4, p1, p2, p3, p4, r1, r2, r3, r4];
 window.onload = function () {
-  // FOR LOOP FOR ALL THE PRODUCTS ON THE FRONT PAGE
+  // FUNCTION FOR ALL THE PRODUCTS ON THE FRONT PAGE
+  products();
 
-  for (let i = 0; i < productInfo.length; i++) {
-    let imgContainer: HTMLDivElement = document.getElementById(
-      "imgcontainer"
-    ) as HTMLDivElement;
-    imgContainer.innerHTML = productInfo[i].imgsmall;
-  }
   // cartClick();
   clicker();
 };
+
+function products() {
+  for (let i = 0; i < productInfo.length; i++) {
+    let wirelessContainer: HTMLDivElement = document.getElementById(
+      "wirelesscontainer"
+    ) as HTMLDivElement;
+
+    // productcontainer div som ska ligga innanför wirelesscontainer
+    let productContainer = document.createElement("div");
+    productContainer.className = "productcontainer";
+
+    // imgcontainer div som ska ligga innanför productContainer
+    let imgContainer = document.createElement("div");
+    imgContainer.className = "imgcontainer";
+    imgContainer.innerHTML = productInfo[i].imgsmall;
+
+    // div som ska ha vacuumheader, vacuuminfo, productbtns
+    let productInfoContainer = document.createElement("div");
+    productInfoContainer.className = "productInfoContainer";
+
+    // html för vacuumheader, vacuuminfo, price, productbtns
+    let vacuumHeader = document.createElement("h2");
+    vacuumHeader.className = "vacuumheader";
+    vacuumHeader.innerHTML = productInfo[i].discriptionlandpageh2;
+
+    let vacuumInfo = document.createElement("div");
+    vacuumInfo.className = "vacuuminfo";
+    vacuumInfo.innerHTML = productInfo[i].discriptionlandpagespan;
+
+    let price = document.createElement("h2");
+    price.className = "price";
+    price.innerHTML = `${productInfo[i].price.toString()} kr`;
+
+    let productBtns = document.createElement("div");
+    productBtns.className = "productbtns";
+
+    let readMoreBtn = document.createElement("button");
+    readMoreBtn.className = "readmore";
+    readMoreBtn.innerHTML = productInfo[i].readmorebtn;
+
+    let addToCartBtn = document.createElement("button");
+    addToCartBtn.className = "addtocart";
+    addToCartBtn.innerHTML = productInfo[i].addtocartbtn;
+
+    //<span></span> innanför vacuuminfo
+    let vacuumInfoSpan = document.createElement("span");
+
+    // appendChild
+    wirelessContainer.appendChild(productContainer);
+    productContainer.appendChild(imgContainer);
+    productContainer.appendChild(productInfoContainer);
+    productInfoContainer.appendChild(vacuumHeader);
+    productInfoContainer.appendChild(vacuumInfo);
+    productInfoContainer.appendChild(price);
+    productInfoContainer.appendChild(productBtns);
+    productBtns.appendChild(readMoreBtn);
+    productBtns.appendChild(addToCartBtn);
+    vacuumInfo.appendChild(vacuumInfoSpan);
+  }
+}
 
 // function cartClick() {
 // 	let cartLink: HTMLLinkElement = document.getElementById(
