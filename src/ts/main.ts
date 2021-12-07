08eec39a1bc19064733357445f64c41edf464cd2
19OZMAN <76961722+19ozman@users.noreply.github.com>
2021-12-07 10:37:52 +0100

    added clicker function as export

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -13,6 +13,7 @@ import {
 	r3,
 	r4,
 } from "./models/Vacuumobjects";
+import { clicker } from "./models/Clicker";
 
 window.onload = function () {
 	// cartClick();
@@ -96,39 +97,6 @@ function products() {
 		productBtns.appendChild(addToCartBtn);
 		vacuumInfo.appendChild(vacuumInfoSpan);
 	}
-}
-
-function clicker() {
-	var modal: HTMLDivElement = document.getElementById(
-		"myModal"
-	) as HTMLDivElement;
-
-	// Get the button that opens the modal
-	var btn: HTMLButtonElement = document.getElementById(
-		"btnCart"
-	) as HTMLButtonElement;
-
-	// Get the <span> element that closes the modal
-	var span: HTMLSpanElement = document.getElementsByClassName(
-		"close"
-	)[0] as HTMLSpanElement;
-
-	// When the user clicks on the button, open the modal
-	btn.onclick = function () {
-		modal.style.display = "block";
-	};
-
-	// When the user clicks on <span> (x), close the modal
-	span.addEventListener("click", function () {
-		modal.style.display = "none";
-	});
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function (event) {
-		if (event.target == modal) {
-			modal.style.display = "none";
-		}
-	};
 }
 
 let cartItems: object[] = [w1];
