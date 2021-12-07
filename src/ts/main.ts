412e689c31e6f5ea10ed009ca49f163fd5dcc2fb
Filippa <filippabellange@live.se>
2021-12-07 09:08:18 +0100

    fix import problem, form problem

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -12,15 +12,19 @@ import {
     r2,
     r3,
     r4 } from "./models/Vacuumobjects";
+
 window.onload = function () {
 
 	// cartClick();
-	clicker();
+  form();
+	// clicker();
   document.getElementById("btnCart").addEventListener("click", displayCart)
+}
 
 let productInfo: Vacuum[] = [w1, w2, w3, w4, p1, p2, p3, p4, r1, r2, r3, r4];
-window.onload = function () {
+function loopproducts () {
   // FOR LOOP FOR ALL THE PRODUCTS ON THE FRONT PAGE
+
 
   for (let i = 0; i < productInfo.length; i++) {
     let imgContainer: HTMLDivElement = document.getElementById(
@@ -29,8 +33,7 @@ window.onload = function () {
     imgContainer.innerHTML = productInfo[i].imgsmall;
   }
   // cartClick();
-  clicker();
-};
+}
 
 // function cartClick() {
 // 	let cartLink: HTMLLinkElement = document.getElementById(
@@ -46,41 +49,42 @@ window.onload = function () {
 // 	});
 // }
 
-function clicker() {
-  var modal: HTMLDivElement = document.getElementById(
-    "myModal"
-  ) as HTMLDivElement;
+// function clicker() {
+//   var modal: HTMLDivElement = document.getElementById(
+//     "myModal"
+//   ) as HTMLDivElement;
 
-  // Get the button that opens the modal
-  var btn: HTMLButtonElement = document.getElementById(
-    "btnCart"
-  ) as HTMLButtonElement;
+//   // Get the button that opens the modal
+//   var btn: HTMLButtonElement = document.getElementById(
+//     "btnCart"
+//   ) as HTMLButtonElement;
 
-  // Get the <span> element that closes the modal
-  var span: HTMLSpanElement = document.getElementsByClassName(
-    "close"
-  )[0] as HTMLSpanElement;
+//   // Get the <span> element that closes the modal
+//   var span: HTMLSpanElement = document.getElementsByClassName(
+//     "close"
+//   )[0] as HTMLSpanElement;
 
-  // When the user clicks on the button, open the modal
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
+//   // When the user clicks on the button, open the modal
+//   btn.onclick = function () {
+//     modal.style.display = "block";
+//   };
 
-  // When the user clicks on <span> (x), close the modal
-  span.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
+//   // When the user clicks on <span> (x), close the modal
+//   span.addEventListener("click", function () {
+//     modal.style.display = "none";
+//   });
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
-}
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function (event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+//   };
+// }
 
 
 let cartItems: object[] = [w1];
+
 function addToCart(clicked) {
 
     for (let i = 0; i < cartItems.length; i++) {
@@ -143,4 +147,23 @@ function displayCart() {
     }
 
 }
+
+function form () {
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
 }
