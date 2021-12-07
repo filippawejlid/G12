7c2814d47435e8382f129b2ef99c48ae7ae4173b
19OZMAN <76961722+19ozman@users.noreply.github.com>
2021-12-07 13:28:42 +0100

    Changes of directory in ts files

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,33 +1,33 @@
-import { displayCart } from "./models/displayCart";
-import { productinfo } from "./models/Productinfo";
-import { products } from "./models/Products";
-import { clicker } from "./models/Clicker";
-import { form } from "./models/Form";
-import { addToCart } from "./models/addToCart";
+import { displayCart } from "./displayCart";
+import { productinfo } from "./Productinfo";
+import { products } from "./Products";
+import { clicker } from "./Clicker";
+import { form } from "./Form";
+import { addToCart } from "./addToCart";
 import { Vacuum } from "./models/Vacuum";
 import {
-  w1,
-  w2,
-  w3,
-  w4,
-  p1,
-  p2,
-  p3,
-  p4,
-  r1,
-  r2,
-  r3,
-  r4,
+	w1,
+	w2,
+	w3,
+	w4,
+	p1,
+	p2,
+	p3,
+	p4,
+	r1,
+	r2,
+	r3,
+	r4,
 } from "./models/Vacuumobjects";
 
 window.onload = function () {
-  // cartClick();
-  //addToCart();
-  clicker();
-  displayCart();
-  form();
-  productinfo();
-  products();
+	// cartClick();
+	//addToCart();
+	clicker();
+	displayCart();
+	form();
+	productinfo();
+	products();
 
-  document.getElementById("btnCart").addEventListener("click", displayCart);
+	document.getElementById("btnCart").addEventListener("click", displayCart);
 };
