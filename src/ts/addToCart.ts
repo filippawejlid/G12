4bcbd7b97faee097c4ea62de4c7384619da2bad9
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2021-12-13 13:36:44 +0100

    minor things

--- src/ts/addToCart.ts
+++ src/ts/addToCart.ts
@@ -2,48 +2,11 @@ import { Vacuum } from "./models/Vacuum";
 import { productInfo } from "./Products";
 // import { minusItem } from "./minusitem";
 
-<<<<<<< HEAD
 localStorage.clear();
-export let cartItems: Vacuum[] =
-	JSON.parse(localStorage.getItem("cartItems")) || [];
-
-export function addToCart(clicked: number) {
-	let productContainer = document.getElementById("objectContainer" + clicked);
-
-	if (cartItems.length == 0) {
-		productInfo[clicked].identification = "objectContainer" + clicked;
-		productInfo[clicked].beenAdded = true;
-		cartItems.push(productInfo[clicked]);
-
-		console.log(
-			"La till " +
-				productInfo[clicked].productName +
-				productInfo[clicked].beenAdded
-		);
-	} else if (productInfo[clicked].beenAdded == true) {
-		for (let i = 0; i < cartItems.length; i++) {
-			if (productContainer.id === cartItems[i].identification) {
-				cartItems[i].amount++;
-
-				console.log("Plussa amount " + cartItems[i].amount);
-				break;
-			}
-		}
-	} else if (productInfo[clicked].beenAdded == false) {
-		productInfo[clicked].identification = "objectContainer" + clicked;
-		cartItems.push(productInfo[clicked]);
-		productInfo[clicked].beenAdded = true;
-	}
-
-	for (let i = 0; i < cartItems.length; i++) {
-		localStorage.setItem("cartItems", JSON.stringify(cartItems));
-	}
-=======
 export let cartItems: Vacuum[] =
   JSON.parse(localStorage.getItem("cartItems")) || [];
 
 export function addToCart(clicked: number) {
-  localStorage.clear();
   let productContainer = document.getElementById("objectContainer" + clicked);
 
   if (cartItems.length == 0) {
@@ -74,5 +37,4 @@ export function addToCart(clicked: number) {
   for (let i = 0; i < cartItems.length; i++) {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
->>>>>>> feature-branch/+-product2
 }
