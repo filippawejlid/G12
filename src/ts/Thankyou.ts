4853ea2689a4e453979261c1e00b97472acf7dcc
19OZMAN <76961722+19ozman@users.noreply.github.com>
2021-12-17 13:17:47 +0100

    minor tweaks

--- src/ts/Thankyou.ts
+++ src/ts/Thankyou.ts
@@ -3,12 +3,4 @@ import { Cart } from "./models/Cart";
 window.onload = function () {
 	let cart = new Cart();
 	cart.thankyouCart();
-	toggleMobileMenu();
 };
-
-function toggleMobileMenu() {
-	let hamburger = document.getElementById("hamburger-icon");
-	hamburger.addEventListener("click", () => {
-		hamburger.classList.toggle("open");
-	});
-}
